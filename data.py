6d04b38e192858c647201b561b6cd7c27bff4156
dldou <cassanum@protonmail.com>
2022-12-13 17:39:15 +0100

    Dataset class to deal with TrayDataset

--- data.py
+++ data.py
@@ -6,18 +6,51 @@ Created on Mon Dec 12 20:55:01 2022
 @author: dldou
 """
 
-import torch
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from PIL import Image
 
 
+#Preparing the Dataset class
+class TrayDataset(Dataset):
 
-def image_labeling(image, threshold):
-    """
-        Apply a threshold to tag the ink 1 and the paper 0
-    """
+    def __init__(self, annotations_file_path, 
+                 _img_dir_path, _label_dir_path,
+                 _transforms = None, _target_transforms = None):
+        super(TrayDataset, self).__init__()
 
-    #Copying tensor
-    label = image.detach().clone()
-    #Tagging
-    label = torch.where(label > threshold, 1, 0)
+        self.labels            = pd.read_csv(annotations_file_path)
+        self.img_dir_path      = _img_dir_path
+        self.label_dir_path    = _label_dir_path
+        self.transforms        = _transforms
+        self.target_transforms = _target_transforms
 
-    return label
+
+    def __len__(self):
+        return len(os.listdir(self.img_dir_path))
+
+
+    def __getitem__(self, img_idx):
+
+        #/!\ NOT THAT GOOD BECAUSE DATA LIST ARE SORTED EVERY TIME WE ACCESS /!\#
+        img_name   = sorted(os.listdir(self.img_dir_path))[img_idx]
+        label_name = sorted(os.listdir(self.label_dir_path))[img_idx]
+
+        #Get the image
+        image_path = self.img_dir_path + img_name
+        image      = Image.open(image_path)
+        if self.transforms:
+            image  = self.transforms(image)
+        #Get the label
+        label_path = self.label_dir_path + label_name
+        label      = Image.open(label_path)
+        if self.target_transforms:
+            label  = self.target_transforms(label)
+
+        return image, label
+
+
