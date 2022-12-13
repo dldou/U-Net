cbaf61074f82bf107600dd1859b2c22e62459f81
dldou <cassanum@protonmail.com>
2022-12-13 17:42:44 +0100

    Updated main

--- main.py
+++ main.py
@@ -19,36 +19,61 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    torch.cuda.empty_cache()
 
-    in_channels = 1 
-    nof_classes = 2
+    #Data
+    transform        = transforms.Compose([transforms.Pad((78,158), fill=0, padding_mode='constant'),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize(mean=0.5, std=0.5)])
+    
+    target_transform = transforms.Compose([transforms.Pad((78,158), fill=0, padding_mode='constant'),
+                                           transforms.ToTensor()])
+    
+    annotations_file_path    = '/content/classes.csv'
+    train_dataset_img_path   = '/content/TrayDataset/TrayDataset/XTrain/'
+    train_dataset_label_path = '/content/TrayDataset/TrayDataset/yTrain/'
+        
+    train_dataset = TrayDataset(annotations_file_path, 
+                                train_dataset_img_path, train_dataset_label_path,
+                                transform, target_transform
+                                )
+
+    annotations_file_path    = '/content/classes.csv'
+    test_dataset_img_path   = '/content/TrayDataset/TrayDataset/XTest/'
+    test_dataset_label_path = '/content/TrayDataset/TrayDataset/yTest/'
+
+    transform        = transforms.Compose([transforms.Pad((78,158), fill=0, padding_mode='constant'),
+                                        transforms.ToTensor()])
+    target_transform = transforms.Compose([transforms.Pad((78,158), fill=0, padding_mode='constant'), 
+                                           transforms.ToTensor()])
+
+    test_dataset = TrayDataset(annotations_file_path, 
+                                test_dataset_img_path, test_dataset_label_path,
+                                transform, target_transform
+                                )
+    #Test Dataloader
+    _batch_size = 1
+
+    train_dataloader = DataLoader(train_dataset, batch_size=_batch_size)
+    test_dataloader = DataLoader(test_dataset, batch_size=_batch_size)
+
+    #Model instanciation
+    in_channels = 3 
+    nof_classes = len(train_dataset) #(has the background class)
 
     Unet = UNet(in_channels, nof_classes)
     Unet.to(device)
 
-    #summary(Unet, (1, 572, 572))
-
-    #Data
-    transform = transforms.Compose([transforms.Pad((272,272), padding_mode='symmetric'),
-                                transforms.ToTensor(),
-                                transforms.Normalize(mean=0.5, std=0.5)
-                               ])
-
-    #Train dataset
-    mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    #Dataloader used to shuffle and create batch
-    train_loader   = torch.utils.data.DataLoader(mnist_trainset, batch_size=1, shuffle=True)
-    #Test dataset
-    mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-    test_loader   = torch.utils.data.DataLoader(mnist_testset, batch_size=1, shuffle=True)
+    #summary(Unet, (3, 572, 572))
 
     #Training
     model = Unet
-    train_loader = train_loader
-    test_loader  = test_loader
+    train_loader = train_dataloader
+    test_loader  = test_dataloader
     nof_epochs = 5
     learning_rate = 0.001
     optimizer = torch.optim.Adam(Unet.parameters(), lr = 0.001)
+    #/!\ Wrong criterion /!\#
     criterion = torch.nn.MSELoss()
     file_path_save_model = '/content/checkpoint.pth'
 
@@ -58,6 +83,4 @@ if __name__ == "__main__":
 
     #Display results
     plot_results(Unet, test_loader, device)
-
-
 
